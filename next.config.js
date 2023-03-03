1fc8384797be9a12b834f0f90a1f1b212fd93fd4
StreamPay <contact@streamprotocol.org>
2023-03-03 11:35:56 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
     features: store.features,
     reactStrictMode: true,
     images: {
-        domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "https://streampay.shop/images", "localhost"],
+        domains: ["ec2-54-157-79-121.compute-1.amazonaws.com", "https://streampay.shop/images", "localhost"],
     },
 })
 
